7ffbb47f6c47bf695ef6606191d417b4f058b968
Martin Atkins <mart@degeneration.co.uk>
2018-01-14 14:28:35 -0800

    eval: Allow use of locals in constant expressions
    We allow the use of locals in constant expressions only if the local
    expression is transitively constant. This allows the convenience of using
    locals to DRY-up some values that are used in a lot of constant contexts,
    while allowing locals to be used in dynamic contexts too where possible.

--- eval/eval_constant.go
+++ eval/eval_constant.go
@@ -20,26 +20,74 @@ import (
 // object is also available for use, exposing the values in the given EachState.
 func (mctx *ModuleContext) EvalConstant(expr hcl.Expression, ty cty.Type, each EachState) (cty.Value, hcl.Diagnostics) {
 	// This method must be careful in how it uses mctx because it is called
-	// during the construction of mctx. The only safe field to use is
-	// mctx.Constants.
+	// during the construction of mctx. The only safe fields to use are
+	// mctx.Constants and mctx.Config.
 
 	var diags hcl.Diagnostics
 	scope := make(map[string]cty.Value)
+	locals := make(map[string]cty.Value)
 
 	traversals := expr.Variables()
 	for _, traversal := range traversals {
-		if rootName := traversal.RootName(); rootName != "Const" {
-			if rootName == "Each" {
-				if each == NoEachState {
-					diags = append(diags, &hcl.Diagnostic{
-						Severity: hcl.DiagError,
-						Summary:  "Illegal use of \"Each\" object",
-						Detail:   "The \"Each\" object can be accessed only within modules and resources that have ForEach set.",
-						Subject:  traversal.SourceRange().Ptr(),
-					})
-				}
-				continue
+		rootName := traversal.RootName()
+		switch rootName {
+		case "Const":
+			// Allowed, but no special action is required here since we just
+			// install the full constant table in the scope below.
+		case "Each":
+			if each == NoEachState {
+				diags = append(diags, &hcl.Diagnostic{
+					Severity: hcl.DiagError,
+					Summary:  "Illegal use of \"Each\" object",
+					Detail:   "The \"Each\" object can be accessed only within modules and resources that have ForEach set.",
+					Subject:  traversal.SourceRange().Ptr(),
+				})
 			}
+		case "Local":
+			// Only locals that are fully-constant are allowed in constant
+			// expressions.
+			if len(traversal) < 2 {
+				diags = append(diags, &hcl.Diagnostic{
+					Severity: hcl.DiagError,
+					Summary:  "Illegal use of Local object",
+					Detail:   "The top-level object \"Local\" requires an attribute to specify which local value to access.",
+					Subject:  traversal.SourceRange().Ptr(),
+				})
+				break
+			}
+			nameStep, ok := traversal[1].(hcl.TraverseAttr)
+			if !ok {
+				// We'll just fall out here so that we'll later produce our
+				// usual message for doing an inappropriate traversal of an
+				// object.
+				break
+			}
+
+			localName := nameStep.Name
+			localAttr, exists := mctx.Config.Locals[localName]
+			if !exists {
+				// We'll just fall out here without setting a value for
+				// this local so that we'll produce our usual message for
+				// the attribute not existing.
+				break
+			}
+
+			if len(mctx.DetectVariables(localAttr.Expr)) != 0 {
+				diags = append(diags, &hcl.Diagnostic{
+					Severity: hcl.DiagError,
+					Summary:  "Illegal use of non-constant value",
+					Detail:   "Can only use locals that have constant values in this context.",
+					Subject:  traversal.SourceRange().Ptr(),
+				})
+				// Put in a placeholder value so we won't generate any more
+				// errors for this one during evaluation.
+				locals[localName] = cty.DynamicVal
+			} else {
+				localVal, localDiags := mctx.EvalConstant(localAttr.Expr, cty.DynamicPseudoType, NoEachState)
+				diags = append(diags, localDiags...)
+				locals[localName] = localVal
+			}
+		default:
 			diags = append(diags, &hcl.Diagnostic{
 				Severity: hcl.DiagError,
 				Summary:  "Illegal use of non-constant value",
@@ -54,6 +102,7 @@ func (mctx *ModuleContext) EvalConstant(expr hcl.Expression, ty cty.Type, each E
 	}
 
 	scope["Const"] = cty.ObjectVal(mctx.Constants)
+	scope["Local"] = cty.ObjectVal(locals)
 	if each != NoEachState {
 		scope["Each"] = cty.ObjectVal(map[string]cty.Value{
 			"Key":   each.Key.Value(),
@@ -98,6 +147,51 @@ func (mctx *ModuleContext) EvalConstant(expr hcl.Expression, ty cty.Type, each E
 	}
 
 	return val, diags
+}
+
+// DetectVariables is like the global function of the same name, except that
+// it additionally follows references to locals and includes them only if
+// they transitively refer to variables.
+func (mctx *ModuleContext) DetectVariables(expr hcl.Expression) []hcl.Traversal {
+	var ret []hcl.Traversal
+	traversals := expr.Variables()
+	for _, traversal := range traversals {
+		switch traversal.RootName() {
+		case "Const", "Each":
+			// We allow both "Const" and "Each" because const refers to named
+			// constants and "Each" is used in ForEach constructs which
+			// require their inputs to be constant.
+			continue
+		case "Local":
+			if len(traversal) < 2 {
+				break
+			}
+			nameStep, ok := traversal[1].(hcl.TraverseAttr)
+			if !ok {
+				break
+			}
+			localAttr, exists := mctx.Config.Locals[nameStep.Name]
+			if !exists {
+				break
+			}
+
+			transitive := mctx.DetectVariables(localAttr.Expr)
+			if len(transitive) == 0 {
+				// If the local's expression doesn't depend on any variables
+				// then we'll omit it from what we return.
+				continue
+			}
+
+			// Otherwise, we'll include just the local reference itself,
+			// and discard the transitive variables, so the caller can
+			// produce reasonable error diagnostics that don't point to
+			// expressions outside of the one that was passed in here.
+		}
+
+		ret = append(ret, traversal)
+	}
+	return ret
+
 }
 
 // DetectVariables returns all of the traversals in the given expression that
